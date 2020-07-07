6518688ebc9f7bd231479e6c8cbc01b0941f1ae4
vladtf <vendeta0263@gmail.com>
2020-07-07 18:18:53 +0300

    Render products from database

--- store/views.py
+++ store/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
+from store.models import *
 
-
-# Create your views here.
 
 def store(request):
-    context = {}
+    products = Product.objects.all()
+    context = {'products': products}
     return render(request, 'store/store.html', context)
 
 
