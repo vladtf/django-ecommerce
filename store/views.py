adaa14ab2e56aa04c8b3bf1ea061b12f77fda352
vladtf <vendeta0263@gmail.com>
2020-07-07 23:34:13 +0300

    Checkout page data

--- store/views.py
+++ store/views.py
@@ -9,6 +9,21 @@ def store(request):
 
 
 def cart(request):
+    context = get_order_data(request)
+    return render(request, 'store/cart.html', context)
+
+
+def checkout(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+
+    context = {'items': items, 'order': order}
+    return render(request, 'store/checkout.html', context)
+
+
+def get_order_data(request):
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
@@ -16,11 +31,5 @@ def cart(request):
     else:
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0}
-
     context = {'items': items, 'order': order}
-    return render(request, 'store/cart.html', context)
-
-
-def checkout(request):
-    context = {}
-    return render(request, 'store/checkout.html', context)
+    return context
