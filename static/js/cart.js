bfe0c2d293902dd62f5169c157b57ffa859c553e
vladtf <vendeta0263@gmail.com>
2020-07-11 20:46:52 +0300

    Add/Remove cart to items

--- static/js/cart.js
+++ static/js/cart.js
@@ -9,11 +9,34 @@ for (var i = 0; i < updateButtons.length; i++) {
 
         console.log('User', user)
         if (user === 'AnonymousUser') {
-            console.log('Not logged in')
+            addCookieItem(productId, action)
         } else {
             updateUserOrder(productId, action)
         }
     })
+}
+
+function addCookieItem(productId, action) {
+    console.log('Not logged in...')
+
+    if (action === 'add') {
+        if (cart[productId] === undefined) {
+            cart[productId] = {'quantity': 1}
+        } else {
+            cart[productId]['quantity'] += 1
+        }
+    } else if (action === 'remove') {
+        cart[productId]['quantity'] -= 1
+
+        if (cart[productId]['quantity'] <= 0) {
+            console.log('Remove item ')
+            delete cart[productId]
+        }
+    }
+
+    console.log('Cart: ', cart)
+    document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
+    location.reload()
 }
 
 function updateUserOrder(productId, action) {
