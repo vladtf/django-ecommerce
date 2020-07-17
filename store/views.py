9507910b31449efc4641c3d22046d8dd4389c25c
vladtf <vendeta0263@gmail.com>
2020-07-17 05:08:56 +0300

    Fixed check price equality

--- store/views.py
+++ store/views.py
@@ -65,8 +65,10 @@ def process_order(request):
     total = float(data['form']['total'])
     order.transaction_id = transaction_id
 
-    if total == order.get_cart_total:
+    if float(order.get_cart_total) == total:
         order.complete = True
+        print(total, '=', order.get_cart_total)
+
     order.save()
 
     if order.shipping == True:
